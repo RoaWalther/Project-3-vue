8c2944d0379b199438daa4b9e30d595e2a20a26a
Walther Roa <roawalter95@gmail.com>
2022-01-29 20:04:39 -0500

    Project-3-vue-fetch

---
+++ src/views/Fotos.vue
@@ -0,0 +1,31 @@
+<template>
+    <div>
+        <h1>Galeria de imagenes</h1>
+        <router-link :to="{name:'Fotos', params: {id: item}}" 
+        v-for="(item,index) of fotos" :key="index">
+            <button>Foto: {{item}}</button>
+        </router-link>
+        <Foto></Foto>
+        <button @click="about">Aboutodo</button>
+    </div>
+</template>
+
+<script>
+import Foto from '@/components/Fotografia.vue'
+export default {
+    name: 'Fotos',
+    components: {
+        Foto
+    },
+    data() {
+        return {
+            fotos:[1,2,3]
+        };
+    },
+    methods: {
+        about(){
+            this.$router.push('/about')
+        }
+    },
+}
+</script>
